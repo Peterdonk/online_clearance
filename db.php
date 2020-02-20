ccc4baf02f5e656762f4bbb23bf9fb934874625a
Peter Donkor <peterdonk17@gmail.com>
2020-02-20 10:05:10 +0000

    db_update

--- db.php
+++ db.php
@@ -1,8 +1,17 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
+// $host = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "clearance";
+
+// $connectionString = mysqli_connect($host,$user,$password,$database);
+
+
+//on test server
+$host = "db4free.net";
+$user = "clearance_userna";
+$password = "clearance_password";
 $database = "clearance";
 
 $connectionString = mysqli_connect($host,$user,$password,$database);
